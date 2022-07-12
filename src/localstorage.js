14ee0e91c6f442812752887c7d65222031e4fc9c
gak92 <44802922+gak92@users.noreply.github.com>
2022-07-12 17:47:04 +0500

    Remove linters error

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,7 +1,5 @@
 export default class LocalStorage {
-  static getData = () => {
-    return JSON.parse(localStorage.getItem('taskslist'));
-  }
+  static getData = () => JSON.parse(localStorage.getItem('taskslist'));
 
   static saveData = (tasksList) => {
     localStorage.setItem('taskslist', JSON.stringify(tasksList));
